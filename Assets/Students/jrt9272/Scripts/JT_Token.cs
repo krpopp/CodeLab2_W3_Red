1b2b640d38ce19402bbd1c41580e5902521988c6
Jon T <jt.starewell@gmail.com>
2022-09-23 11:10:24 -0400

    code commented, two bonus scenes utilizing system

--- Assets/Students/jrt9272/Scripts/JT_Token.cs
+++ Assets/Students/jrt9272/Scripts/JT_Token.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class JT_Token : MonoBehaviour {
 
+    //enum for gravity shift value
     public enum GravityShift { none, up, down, left, right };
     public GravityShift gShift;
-    public enum TokenColor { red, yellow, green, blue, purple };
+
+    //enum for matching colors
+    public enum TokenColor { red, yellow, green, blue, purple, orange };
     public TokenColor color;
+
+    //enum for visuals
     public enum TokenGraphic { circle, arrow };
     public TokenGraphic graphic;
-
+    //Vector storing position in token array
     public Vector2 coord;
 
+    //Randomly assign direction if arrow
+    void Start() { 
+        if (graphic == TokenGraphic.arrow) {
+            int dir = Random.Range(0, 4);
+            switch (dir) {
+                default:
+                    break;
+                case 0:
+                    gShift = GravityShift.left;
+                    break;
+                case 1:
+                    gShift = GravityShift.up;
+                    gameObject.transform.eulerAngles = new Vector3(0, 0, 270);
+                    break;
+                case 2:
+                    gShift = GravityShift.right;
+                    gameObject.transform.eulerAngles = new Vector3(0, 0, 180);
+                    break;
+                case 3:
+                    gShift = GravityShift.down;
+                    gameObject.transform.eulerAngles = new Vector3(0, 0, 90);
+                    break;
+            }
+        }
+    }
 }
---
+++ Assets/Students/jrt9272/Scripts/Week1WarmUpScripts/JT_CellVisuals.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JT_CellVisuals : MonoBehaviour
+{
+    
+    //Visual references
+    public GameObject slice, rightWall, leftWall, backWall, obj;
+    public List<GameObject> rightHallway, leftHallway;
+    [SerializeField]
+    Sprite[] objColors;
+    int colorIndex;
+
+    //Toggles for what should be made visible
+    //hidden, openLeft, openRight, closed, object
+    bool[] hidden = { false, false, false, false, true };
+
+    //Store paramaters locally. Wouldn't need to if I could pass paramaters through public coroutines
+    public void UpdateVisuals(bool[] paramaters, int cI) {
+        hidden = paramaters;
+        colorIndex = cI;
+        StartCoroutine(FakeScan());
+    }
+
+    //Coroutine which animates the visual updates, makes a fake scan
+    IEnumerator FakeScan() {
+        //Hide everything
+        slice.SetActive(false);
+        yield return new WaitForSeconds(.05f); //Delay to draw new slice
+
+        if (!hidden[0]) { //If there's something to draw
+            slice.SetActive(true);
+
+            if (hidden[1]) { //Toggles Left Hallway
+                leftWall.SetActive(false);
+                foreach(GameObject go in leftHallway) go.SetActive(true);             
+            } else { 
+                leftWall.SetActive(true);
+                foreach(GameObject go in leftHallway) go.SetActive(false);               
+            }
+
+            if (hidden[2]) { //Toggles Right Hallway
+                rightWall.SetActive(false);
+                foreach(GameObject go in rightHallway) go.SetActive(true);                
+            } else { 
+                rightWall.SetActive(true);
+                foreach(GameObject go in rightHallway) go.SetActive(false);           
+            }
+
+            if (hidden[3]) { //Toggles Back Wall
+                backWall.SetActive(true);
+            } else {
+                backWall.SetActive(false);
+            }
+
+            if (hidden[4]) { //Toggles object displayed
+                obj.SetActive(true);
+                obj.GetComponent<SpriteRenderer>().sprite = objColors[colorIndex];
+            } else obj.SetActive(false);
+
+        } else { //There's nothing to draw
+            slice.SetActive(false);
+        }
+    }
+}
